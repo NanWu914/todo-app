3f4b1fbd16055a8672da249cd09959dd173a30d5
floria914 <floriawu914@gmail.com>
2023-08-28 20:23:18 +0200

    Implement the Add Button

--- cli.py
+++ cli.py
@@ -11,13 +11,13 @@ while True:
     if user_action.startswith('add'):
         todo=user_action[4:]
 
-        todos=functions.get_todos('todos.txt')
+        todos=functions.get_todos()
 
         todos.append(todo)
-        functions.write_todos('todos.txt',todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos=functions.get_todos('todos.txt')
+        todos=functions.get_todos()
 
         new_todos = [item.strip("\n") for item in todos]
         for index,item in enumerate(new_todos):
@@ -29,12 +29,12 @@ while True:
             number = int(user_action[5:])
             number=number-1
 
-            todos=functions.get_todos('todos.txt')
+            todos=functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            functions.write_todos('todos.txt', todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Command is not valid.")
             continue
@@ -42,12 +42,12 @@ while True:
     elif user_action.startswith('complete'):
         try:
             number= int(user_action[9:])
-            todos=functions.get_todos('todos.txt')
+            todos=functions.get_todos()
             index=number - 1
             todo_to_remove=todos[index].strip('\n')
             todos.pop(index)
 
-            functions.write_todos('todos.txt',todos)
+            functions.write_todos(todos)
             message=f"Todo {todo_to_remove} was removed from the list."
             print(message)
         except IndexError:
