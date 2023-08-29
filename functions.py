7d3c834fa6d9ff834a8306dcf9b9b72dfc8dae13
floria914 <floriawu914@gmail.com>
2023-08-29 14:56:23 +0200

    Change theme, add clock, handle edit and complete error

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ FILEPATH='todos.txt'
 
 def get_todos(filepath=FILEPATH):
     """Read a text file and return
-    the list of todo items.
+    the list of to-do items.
     """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
